ef47ae1d4b16ca7c99f4479ba50df6aa0ca6477d
Leonid-98 <leonid98@ut.ee>
2022-12-06 22:20:03 +0200

    Created debug plugin

--- include/robotont_driver/plugin_debug.h
+++ include/robotont_driver/plugin_debug.h
@@ -2,12 +2,18 @@
 #define PLUGIN_DEBUG_
 
 #include <ros/ros.h>
-#include <std_msgs/Float32MultiArray.h> 
+#include <std_msgs/Float32MultiArray.h>
 #include "robotont_driver/plugin_base.h"
+
+#define DEBUG_ARGS 10
 
 /**
  * \brief PluginDebug class
  * This plugin subscribes to debug_in topic and relays any debug data as array to the mbed board.
+ * 
+ * Plugin reads data from board in format "DEBUG_OUT:arg1,...argN\r\n", where amount of args defined in
+ * the DEBUG_MAX_ARGS. Next step, publishes received arrays as std_msgs/Float32MultiArray message.
+ * Also, plugin publishes data in the same format to the board.
  */
 namespace robotont
 {
@@ -17,12 +23,20 @@ namespace robotont
         PluginDebug(HardwarePtr hw_ptr, const std::string &name);
         ~PluginDebug();
 
-    private:
-        void writeDebugData(float data[], uint8_t size);
-        void debug_in_callback(const std_msgs::Float32MultiArray& debug_msg);
+        void packetReceived(const RobotontPacket &packet);
 
+    private:
+        // Send data to board
+        void writeDebugData(float data[]);
+        void debug_in_callback(const std_msgs::Float32MultiArray &debug_msg);
+        // Receive data from board
+        void reset();
+        void publish();
+
+        ros::NodeHandle nh_;
         ros::Subscriber debug_sub_;
+        ros::Publisher debug_pub_;
+        std_msgs::Float32MultiArray debug_msg_;
     };
 } // namespace robotont
-
 #endif
--- src/plugin_debug.cpp
+++ src/plugin_debug.cpp
@@ -4,18 +4,28 @@ namespace robotont
 {
     PluginDebug::PluginDebug(HardwarePtr hw_ptr, const std::string &name) : PluginBase(hw_ptr, name)
     {
-        debug_sub_ = nh_.subscribe("cmd_vel", 1, &PluginDebug::debug_in_callback, this);
+        std::string odom_frame;
+        std::string odom_child_frame;
+
+        nh_.param("odom/frame", odom_frame, std::string("odom"));
+        nh_.param("odom/child_frame", odom_child_frame, std::string("base_footprint"));
+        reset();
+
+        // Initialize debug pub and sub
+        debug_pub_ = nh_.advertise<std_msgs::Float32MultiArray>("debug_out", 2);
+        debug_sub_ = nh_.subscribe("debug_in", 1, &PluginDebug::debug_in_callback, this);
     }
 
     PluginDebug::~PluginDebug()
     {
     }
 
-    void PluginDebug::writeDebugData(float data[], uint8_t size)
+    // Send data to board
+    void PluginDebug::writeDebugData(float data[])
     {
         RobotontPacket packet;
         packet.push_back("DEBUG_IN");
-        for (uint8_t i = 0; i < size; i++)
+        for (uint8_t i = 0; i < DEBUG_ARGS; i++)
         {
             packet.push_back(std::to_string(data[i]));
         }
@@ -28,7 +38,42 @@ namespace robotont
 
     void PluginDebug::debug_in_callback(const std_msgs::Float32MultiArray &debug_msg)
     {
-        writeDebugData(debug_msg.data, sizeof(debug_msg.data)); // ! HELP
+        writeDebugData(debug_msg.data); // ! HELP 1
+    }
+
+    // Receive data from board
+    void PluginDebug::reset()
+    {
+        debug_msg_.data = {0};
+    }
+
+    void PluginDebug::packetReceived(const RobotontPacket &packet)
+    {
+        if (packet[0] != "DEBUG_OUT")
+        {
+            return;
+        }
+
+        float data_out[DEBUG_ARGS]; // ! HELP 3 / sizeof(packet)
+        for (uint8_t i = 1; i < DEBUG_ARGS + 1; i++)
+        {
+            try
+            {
+                data_out[i] = std::stof(packet[i]);
+            }
+            catch (std::exception e)
+            {
+                ROS_ERROR_THROTTLE(1, "Failed to parse odom packet: %s", e.what());
+            }
+        }
+
+        debug_msg_.data = data_out; // ! HELP 2
+        publish();
+    }
+
+    void PluginDebug::publish()
+    {
+        debug_pub_.publish(debug_msg_);
     }
 
 } // namespace robotont
